eaf9ba08a4287f799a83e1eb02ddc7e469cbbcf0
Joseph Pereniguez <joseph.pereniguez@wooclap.com>
2023-08-11 17:44:53 +0200

    Chore: first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,92 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: process.env.PRISMIC_REPOSITORY_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        schemas: {
+          // list of Prismic document schemas
+          homepage: require("./src/schemas/homepage.json"),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: false,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: process.env.WEBSITE_BASE_URL,
+        sitemap: `${process.env.WEBSITE_BASE_URL}/sitemap-index.xml`,
+        policy: [
+          {
+            userAgent: "*",
+            allow: "/",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: process.env.WEBSITE_NAME,
+        short_name: process.env.WEBSITE_NAME,
+        start_url: `/`,
+        lang: "fr",
+        description: process.env.WEBSITE_DESCRIPTION,
+        background_color: process.env.WEBSITE_THEME_COLOR,
+        theme_color: process.env.WEBSITE_THEME_COLOR,
+        display: `minimal-ui`,
+        icon: `src/images/logo.png`,
+      },
+    },
+  ],
+}
---
+++ src/components/seo.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+function Seo({ description, title, children }) {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default Seo
