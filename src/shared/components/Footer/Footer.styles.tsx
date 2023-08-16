4d0d8b23d22aa27ec7c3c291866ad07e81e6595a
Joseph Pereniguez <joseph.pereniguez@wooclap.com>
2023-08-16 18:37:56 +0200

    Chore: fix missing styles

--- src/shared/components/Footer/Footer.styles.tsx
+++ src/shared/components/Footer/Footer.styles.tsx
@@ -1,16 +1,15 @@
 import { styled } from 'styled-components';
 
-import { Color, Spacer } from '../../constants/constants';
+import { Spacer } from '../../constants/constants';
 
 export const Footer = styled.div`
   display: flex;
   align-content: center;
   justify-content: center;
-  margin: ${Spacer.HUGE} ${Spacer.NONE} ${Spacer.XL} ${Spacer.NONE};
+  margin: ${Spacer.NONE} ${Spacer.XL} ${Spacer.NONE};
 `;
 
 export const Link = styled.a`
   margin-left: ${Spacer.S};
-  color: ${Color.GOLD};
   text-decoration: underline;
 `;
