27b263bb7fa18a6ec804c15fe918f30018c2cc4b
Joseph Pereniguez <joseph.pereniguez@wooclap.com>
2023-08-16 18:22:47 +0200

    Build: adding dsg and ssr

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,17 +7,7 @@ require("dotenv").config({
  */
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
+    `gatsby-plugin-netlify`, 
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
---
+++ src/templates/DSGLayout/DSGLayout.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const DSGLayout = () => {
+    return <></>
+}
+
+export default DSGLayout;
